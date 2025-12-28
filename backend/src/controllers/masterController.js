b0519fdb5cf89153804687bf3f145b372a722d5f
rafikaindah <rafikaindah2003@gmail.com>
2025-12-28 23:23:48 +0700

    Sekolah menambahkan uuid

--- backend/src/controllers/masterController.js
+++ backend/src/controllers/masterController.js
@@ -3,9 +3,14 @@ const pool = require('../config/db'); //mengimpor koneksi database
 // ------------------ SEKOLAH ------------------
 exports.getSekolah = async (req, res) => { //mendapatkan semua data sekolah
   try { // mencoba menjalankan kode berikut
-    const [rows] = await pool.query('SELECT * FROM sekolah'); //menjalankan query untuk mendapatkan semua data sekolah
+    const [rows] = await pool.query(
+      `SELECT id_sekolah, uuid, nama_sekolah, alamat, telepon, created_at
+       FROM sekolah
+       ORDER BY nama_sekolah`
+    ); //menjalankan query untuk mendapatkan semua data sekolah
     res.json(rows); //mengirim respons dengan data sekolah dalam format JSON
   } catch (err) { //menangani kesalahan
+    console.error("getSekolah error:", err);
     res.status(500).json({ message: 'Gagal mengambil data sekolah' }); //mengirim respons 500 dengan pesan error
   }
 };
@@ -17,8 +22,17 @@ exports.createSekolah = async (req, res) => { //membuat data sekolah baru
       'INSERT INTO sekolah (nama_sekolah, alamat, telepon) VALUES (?, ?, ?)', //query untuk memasukkan data sekolah baru
       [nama_sekolah, alamat, telepon] //nilai yang akan dimasukkan
     );
-    res.status(201).json({ id_sekolah: result.insertId }); //mengirim respons 201 dengan id sekolah yang baru dibuat
+    const [rows] = await pool.query(
+      `SELECT id_sekolah, uuid FROM sekolah WHERE id_sekolah = ? LIMIT 1`,
+      [result.insertId]
+    );
+    res.status(201).json({ 
+      message: "Sekolah berhasil ditambahkan",
+      id_sekolah: rows[0].id_sekolah,
+      uuid: rows[0].uuid,
+     }); //mengirim respons 201 dengan id sekolah yang baru dibuat
   } catch (err) {
+    console.error("createSekolah error:", err);
     res.status(500).json({ message: 'Gagal menambah sekolah' }); //mengirim respons 500 dengan pesan error
   }
 };
@@ -33,6 +47,7 @@ exports.updateSekolah = async (req, res) => { //memperbarui data sekolah berdasa
     );
     res.json({ message: 'Sekolah diperbarui' }); //mengirim respons sukses
   } catch (err) { //menangani kesalahan
+    console.error("updateSekolah error:", err);
     res.status(500).json({ message: 'Gagal update sekolah' }); //mengirim respons 500 dengan pesan error
   }
 };
@@ -43,6 +58,7 @@ exports.deleteSekolah = async (req, res) => {//menghapus data sekolah berdasarka
     await pool.query('DELETE FROM sekolah WHERE id_sekolah=?', [id]); //menjalankan query untuk menghapus data sekolah berdasarkan id
     res.json({ message: 'Sekolah dihapus' }); //mengirim respons sukses
   } catch (err) { //menangani kesalahan
+    console.error("deleteSekolah error:", err);
     res.status(500).json({ message: 'Gagal hapus sekolah' }); //mengirim respons 500 dengan pesan error
   }
 };
